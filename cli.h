c4859bc6e1a8de285596a83e7209238163c6eb5f
anst-foto <starininandrey@gmail.com>
2021-01-18 20:17:48 +0300

    Добавление функции приветствия пользователя

---
+++ cli.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+void Welcome()
+{
+    cout << "====================" << endl;
+    cout << "=== БД студентов ===" << endl;
+    cout << "=== ------------ ===" << endl;
+    cout << "= (c) IT Step ======" << endl;
+    cout << "====================" << endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "Student.h"
+#include "cli.h"
 
 using namespace std;
 
 int main()
 {
-    
+    system("chcp 65001");
+
+    Welcome();
 }
