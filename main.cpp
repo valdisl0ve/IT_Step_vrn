fe991c9f93045d97614fd228bab8594eef4d287a
anst-foto <starininandrey@gmail.com>
2021-01-18 20:52:52 +0300

    Добавление обработки ввода пользователем пункта меню

--- main.cpp
+++ main.cpp
@@ -9,4 +9,36 @@ int main()
     system("chcp 65001");
 
     Welcome();
+
+    int choice;
+    bool flagExit = false;
+    do {
+        ShowMenu();
+        cout << endl << "Введите пункт меню: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 0:
+                flagExit = true;
+                break;
+            default:
+                break;
+        }
+    } while (!flagExit);
+    //TODO подумать о создании функции выхода из программы
+    cout << "До свидания!" << endl;
 }
